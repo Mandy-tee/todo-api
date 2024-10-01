29f364d964cfff583648c893b177825bd7cb3ba8
Amanda Tagoe <nayork77@gmail.com>
2024-10-01 12:22:21 +0000

    updated todos

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 import express from "express";
+import todoRouter from "./routes/todo.js";
 
 // Create an express app
 const app = express();
 
-// Define routes
-app.get("/hello", function(req, res, next) {
-    console.log(req.headers);
-    res.json("You visited the hello endpoint");
-});
-
-app.get("/goodbye", function(req, res, next) {
-    console.log(req.query);
-    res.json("See you later");
-});
+// Use routes
+app.use(todoRouter);
 
 // Listen for incoming requests
-app.listen(3000, function() {
+app.listen(3000, () => {
     console.log("App is listening on port 3000");
 });
