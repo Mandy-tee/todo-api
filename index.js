cecf41e31ad154b5243ad796cb3ed968692a1ae6
Amanda Tagoe <nayork77@gmail.com>
2024-10-03 11:44:38 +0000

    update2

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
 import todoRouter from "./routes/todo.js";
+import userRouter from "./routes/user.js";
+
+// Connect to database
+await mongoose.connect(process.env.MONGO_URI);
 
 // Create an express app
 const app = express();
 
+//Use middlewares
+app.use(express.json());
+
 // Use routes
 app.use(todoRouter);
+app.use(userRouter);
 
 // Listen for incoming requests
 app.listen(3000, () => {
-    console.log("App is listening on port 3000");
+  console.log("App is listening on port 3000");
 });
---
+++ routes/user.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { login, logout, register,  } from "../controllers/user.js";
+
+// Create router
+const userRouter = Router();
+
+// Define routes
+userRouter.post("/users/register", register);
+
+userRouter.post("/users/login", login);
+
+userRouter.post("/users/logout", logout);
+
+// Export router
+export default userRouter;
